6c5ef2a77adcc8818bb5b4d296680e3f4408d685
marcomartinscastanho <marco.castanho@gmail.com>
2020-08-19 14:51:30 +0200

    Add supoprt for creating superusers Also added rule for flake8 to accept line length of 120 (personal preference). Also commented an import in admin.py for flake8 not to complain about unused imports.

--- app/core/models.py
+++ app/core/models.py
@@ -17,6 +17,16 @@ class UserManager(BaseUserManager):
 
         return user
 
+    def create_superuser(self, email, password):
+        """Creates and saves a new superuser"""
+
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom User model that supports using e-mail instead of username"""

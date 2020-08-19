d5becc6d75e4f2a0f4b9fe889efa23967e35c165
marcomartinscastanho <marco.castanho@gmail.com>
2020-08-19 14:51:30 +0200

    Add supoprt for creating superusers Also added rule for flake8 to accept line length of 120 (personal preference). Also commented an import in admin.py for flake8 not to complain about unused imports.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -29,6 +29,18 @@ class ModelTests(TestCase):
     def test_new_user_invalid_email(self):
         """Test creating user with no email raises error"""
 
+        # assertions
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, 'test-123')
 
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+
+        user = get_user_model().objects.create_superuser(
+            'test@fake.com',
+            "test-123"
+        )
+
+        # assertions
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
